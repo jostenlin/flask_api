3806ef18602041245af8540c42b299e3b6110083
jostenlin <jostenlin@gmail.com>
2023-06-21 22:22:45 +0800

    hi

--- user.py
+++ user.py
@@ -1,53 +1,39 @@
 from flask_restful import Resource
 from flask import request
-from flask_jwt_extended import jwt_required
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-users = [
-    {
-        'id': 1,
-        'name': 'John Doe',
-        'email': '1@abc.com'
-    },
-    {
-        'id': 2,
-        'name': 'Jane Doe',
-        'email': '2@abc.com'
-    }
-]
+# 初始化 Firebase
+cred = credentials.Certificate('key.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-# 需要整個類別都認證者繼承這個類別
-class JwtProtectedResource(Resource):
-    method_decorators = [jwt_required()]
-
-# 整個類別都需要 JWT 認證
-class Users(JwtProtectedResource):
-    # 返回所有使用者
+class Users(Resource):
     def get(self):
+        # 取得 users 集合的所有文件
+        users_ref = db.collection("users")
+        users = [doc.to_dict() for doc in users_ref.stream()]
         return users
 
 class User(Resource):
-    
-    # 只有這個方法需要 JWT 認證    
-    @jwt_required()
     def get(self):
-        # 取得使用者傳過來的id
         id = request.args.get('id')
-        for user in users:
-            if user['id'] == int(id):
-                return user
-        return {'status': 'failure', 'message': 'User not found'}, 404
+        # 取得特定使用者的文件
+        user_doc = db.collection("users").document(id).get()
+        if user_doc.exists:
+            return user_doc.to_dict()
+        else:
+            return {'status': 'failure', 'message': 'User not found'}, 404
 
     def post(self):
-        # 取得使用者傳過來的json
         user = request.get_json()
-
+        id = user['id']
         # 檢查使用者是否已經存在
-        for u in users:
-            if u['id'] == user['id']:
-                return {'status': 'failure', 'message': 'User already exists'}, 400
-
-        # 新增使用者
-        users.append(user)
+        user_doc = db.collection("users").document(id).get()
+        if user_doc.exists:
+            return {'status': 'failure', 'message': 'User already exists'}, 400
+        # 新增使用者到資料庫
+        db.collection("users").document(id).set(user)
         result = {
             'status': 'success',
             'message': 'User added successfully',
@@ -56,47 +42,33 @@ class User(Resource):
         return result
 
     def delete(self):
-        # 取得使用者傳過來的id
         id = request.args.get('id')
-        for user in users:
-            if user['id'] == int(id):
-                users.remove(user)
-                result = {
-                    'status': 'success',
-                    'message': 'User deleted',
-                    'deleted_user': user
-                }
-                return result
-        return {'status': 'failure', 'message': 'User not found'}, 404
+        # 檢查使用者是否存在
+        user_doc = db.collection("users").document(id).get()
+        if user_doc.exists:
+            # 刪除使用者
+            db.collection("users").document(id).delete()
+            result = {
+                'status': 'success',
+                'message': 'User deleted',
+                'deleted_user': user_doc.to_dict()
+            }
+            return result
+        else:
+            return {'status': 'failure', 'message': 'User not found'}, 404
 
     def put(self):
-        # 取得使用者傳過來的json
         user = request.get_json()
-        for i in range(len(users)):
-            if users[i]['id'] == user['id']:
-                users[i] = user
-                result = {
-                    'status': 'success',
-                    'message': 'User updated successfully',
-                    'updated_user': user
-                }
-                return result
-        return {'status': 'failure', 'message': 'User not found'}, 404
-
-
-
-
-'''
-我們使用 flask_jwt_extended 模組的 JWTManager 來管理 JWT 認證。
-首先，我們設定了 JWT 的金鑰 JWT_SECRET_KEY，你需要將其修改為一個安全且隨機的字串。
-接下來，我們在 Users 和 User 類別的方法上使用了 @jwt_required 裝飾器，
-這表示這些方法需要 JWT 認證才能訪問。最後，我們新增了一個 /login 的路由，
-用於驗證使用者的帳號密碼並產生 JWT Token。
-
-請確保你已經安裝了 Flask-JWT-Extended 套件，可以使用以下命令進行安裝：
-pip install flask-jwt-extended
-請注意，這只是一個示例程式碼，實際的 JWT 實作可能需要更多的安全措施，
-例如使用 HTTPS 進行傳輸、適當的 Token 過期時間等。
-請仔細閱讀 Flask-JWT-Extended 的文件以瞭解更多詳細信息。
-
-'''
+        id = user['id']
+        # 檢查使用者是否存在
+        user_doc = db.collection("users").document(id).get()
+        if not user_doc.exists:
+            return {'status': 'failure', 'message': 'User not found'}, 404
+        # 更新使用者資訊
+        db.collection("users").document(id).set(user)
+        result = {
+            'status': 'success',
+            'message': 'User updated successfully',
+            'updated_user': user
+        }
+        return result
