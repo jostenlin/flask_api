d7c557e3e9cb5f4ac9dc13027c9427f96ff50ece
jostenlin <jostenlin@gmail.com>
2023-06-21 21:20:05 +0800

    hi

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import datetime
+
 # 配置文件
-class Config(object):
+class Config:
     DEBUG = True
     SECRET_KEY = 'your_secret_key'
     DATABASE_URI = 'your_database_uri'
+    
+    JWT_SECRET_KEY = 'super-secret'
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30)
+    
