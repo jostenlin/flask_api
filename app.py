4cfef4e05f6356a245cd8ddd0477201249ea1dec
jostenlin <jostenlin@gmail.com>
2023-06-26 21:14:38 +0800

    hi

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask_restful import Api
 from auth import AuthRoutes
 from config import Config
 
-# 1.obj array          2.sqlite3              3.firestore  
+# 1.obj array          2.sqlite3              3.firestore
 # 4.obj array + jwt    5.google sheets api
 from user5 import Users, User
 from login2 import Login2
+from asyncRoutes import AsyncRoutes
 
 app = Flask(__name__)
 
@@ -24,18 +25,22 @@ AuthRoutes.configure_routes(app)
 
 # 設定其他路由
 api = Api(app)
-api.add_resource(Users, '/users')
-api.add_resource(User, '/user')
-api.add_resource(Login2, '/login2')
+api.add_resource(Users, "/users")
+api.add_resource(User, "/user")
+api.add_resource(Login2, "/login2")
+api.add_resource(AsyncRoutes, "/getAsyncRoutes")
+
 
 # 測試用路由(可刪除)
-@app.route('/')
+@app.route("/")
 def hello_world():
     # 讀取config.py的配置
-    app.config.from_object('config.Config')
+    app.config.from_object("config.Config")
     from flask import current_app
-    # return {'DEBUG': current_app.config['DEBUG']}
-    return {'DEBUG': current_app.config['JWT_SECRET_KEY']}
 
-if __name__ == '__main__':
+    # return {'DEBUG': current_app.config['DEBUG']}
+    return {"DEBUG": current_app.config["JWT_SECRET_KEY"]}
+
+
+if __name__ == "__main__":
     app.run(debug=True)
